9a88a241e7dd0ac6a3a168e60f413c6566762ef9
The-Mag-Pie <barteksor2000@onet.pl>
2023-08-02 22:51:23 +0200

    added validation in select device page popup

--- RaspiRemote/Popups/DevicePopupBase.xaml.cs
+++ RaspiRemote/Popups/DevicePopupBase.xaml.cs
@@ -19,11 +19,45 @@ public partial class DevicePopupBase : Popup
 
     protected void SaveBtnClicked(object sender, EventArgs e)
     {
-        Close(_device);
+        if (ValidateData())
+        {
+            Close(_device);
+        }
     }
 
     protected void CancelBtnClicked(object sender, EventArgs e)
     {
         Close();
+    }
+
+    private bool ValidateData()
+    {
+        var message = string.Empty;
+
+        if (nameEntry.Text is null || nameEntry.Text == string.Empty)
+            message += "Device name is empty.\n";
+
+        if (hostEntry.Text is null || hostEntry.Text == string.Empty)
+            message += "Device host is empty.\n";
+
+        if (portEntry.Text == string.Empty || int.TryParse(portEntry.Text, out var port) is false ||
+            port < 0 || port > 65535)
+            message += "Device port is invalid or empty.\n";
+
+        if (usernameEntry.Text is null || usernameEntry.Text == string.Empty)
+            message += "Device username is empty.\n";
+
+        if (passwordEntry.Text is null || passwordEntry.Text == string.Empty)
+            message += "Device password is empty.\n";
+
+        if (message != string.Empty)
+        {
+            Application.Current.MainPage.DisplayAlert("Error", message, "OK");
+            return false;
+        }
+        else
+        {
+            return true;
+        }
     }
 }
