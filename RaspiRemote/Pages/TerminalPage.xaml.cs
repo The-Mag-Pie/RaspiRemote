9a89a3908ab82e93fc4ed6176cb57b4367c518b3
The-Mag-Pie <barteksor2000@onet.pl>
2023-08-06 02:25:56 +0200

    show the output on the screen + invoke commands from input + fixed problem with special characters by using base64

--- RaspiRemote/Pages/TerminalPage.xaml.cs
+++ RaspiRemote/Pages/TerminalPage.xaml.cs
@@ -8,6 +8,18 @@ public partial class TerminalPage : ContentPage
 	{
 		InitializeComponent();
 
-		BindingContext = ServiceHelper.GetService<TerminalPageViewModel>();
+		var vm = ServiceHelper.GetService<TerminalPageViewModel>();
+		BindingContext = vm;
+
+		consoleWebview.Navigated += (s, e) =>
+		{
+			vm.ConsoleDataReceived += WriteToConsole;
+		};
+    }
+
+    private void WriteToConsole(string content)
+	{
+		var base64content = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(content));
+		consoleWebview.Eval($"writeToConsole(`{base64content}`);");
 	}
 }
--- RaspiRemote/ViewModels/TerminalPageViewModel.cs
+++ RaspiRemote/ViewModels/TerminalPageViewModel.cs
@@ -1,6 +1,57 @@
-﻿namespace RaspiRemote.ViewModels
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Renci.SshNet;
+
+namespace RaspiRemote.ViewModels
 {
     internal partial class TerminalPageViewModel : BaseViewModel
     {
+        private readonly SshClient _sshClient;
+        private readonly ShellStream _shellStream;
+
+        public event Action<string> ConsoleDataReceived;
+
+        [ObservableProperty]
+        private string _commandText;
+
+        public TerminalPageViewModel(SshClientContainer sshClientContainer)
+        {
+            _sshClient = sshClientContainer.SshClient;
+            _shellStream = _sshClient.CreateShellStream("xterm", 0, 0, 0, 0, 0);
+
+            _shellStream.DataReceived += (s, e) =>
+            {
+                if (_shellStream.DataAvailable)
+                {
+                    var data = _shellStream.Read();
+                    ConsoleDataReceived?.Invoke(data);
+                }
+            };
+
+            _shellStream.ErrorOccurred += (s, e) =>
+            {
+                _ = DisplayAlert("Error", e.Exception.Message, "OK");
+            };
+
+            Task.Run(() =>
+            {
+                while (ConsoleDataReceived is null)
+                {
+                    // waiting
+                }
+                _shellStream.WriteLine("");
+            });
+        }
+
+        [RelayCommand]
+        private void Execute()
+        {
+            try
+            {
+                _shellStream.WriteLine(CommandText);
+                CommandText = string.Empty;
+            }
+            catch { }
+        }
     }
 }
