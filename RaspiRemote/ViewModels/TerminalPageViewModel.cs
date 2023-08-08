5c18b45ef37291ac7d29fde5ef8816ba720f73e9
The-Mag-Pie <barteksor2000@onet.pl>
2023-08-08 02:36:31 +0200

    terminal page: implemented arrow, tab and esc keys handling

--- RaspiRemote/ViewModels/TerminalPageViewModel.cs
+++ RaspiRemote/ViewModels/TerminalPageViewModel.cs
@@ -14,6 +14,9 @@ namespace RaspiRemote.ViewModels
         [ObservableProperty]
         private string _commandText;
 
+        [ObservableProperty]
+        private bool _sendCtrlKey;
+
         public TerminalPageViewModel(SshClientContainer sshClientContainer)
         {
             _sshClient = sshClientContainer.SshClient;
@@ -57,12 +60,28 @@ namespace RaspiRemote.ViewModels
         }
 
         [RelayCommand]
-        private void Send(string cmdText)
+        private void SendLine(string cmdText)
         {
             try
             {
                 _shellStream.WriteLine(cmdText);
                 CommandText = string.Empty;
+            }
+            catch (ObjectDisposedException)
+            {
+                _ = HandleShellInterrupted();
+            }
+            catch (Exception ex)
+            {
+                _ = DisplayError(ex.Message);
+            }
+        }
+
+        private void Send(string cmdText)
+        {
+            try
+            {
+                _shellStream.Write(cmdText);
             }
             catch (ObjectDisposedException)
             {
@@ -82,5 +101,26 @@ namespace RaspiRemote.ViewModels
                 _ = ConfigureShellStream();
             }
         }
+
+        [RelayCommand]
+        private void Up() => Send("\x1B[A");
+
+        [RelayCommand]
+        private void Down() => Send("\x1B[B");
+
+        [RelayCommand]
+        private void Left() => Send("\x1B[D");
+
+        [RelayCommand]
+        private void Right() => Send("\x1B[C");
+
+        [RelayCommand]
+        private void Tab() => Send(((char)0x09).ToString());
+
+        [RelayCommand]
+        private void Esc() => Send(((char)0x1B).ToString());
+
+        [RelayCommand]
+        private void ChangeSendCtrlKey() => SendCtrlKey = !SendCtrlKey;
     }
 }
