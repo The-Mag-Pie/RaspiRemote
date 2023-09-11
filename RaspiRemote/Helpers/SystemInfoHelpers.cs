df953d937d86fb239b9b3b08e8cc75481563c326
The-Mag-Pie <barteksor2000@onet.pl>
2023-09-11 02:21:05 +0200

    system info page: implemented basic info

---
+++ RaspiRemote/Helpers/SystemInfoHelpers.cs
@@ -0,0 +1,43 @@
+﻿using Renci.SshNet;
+using Renci.SshNet.Common;
+
+namespace RaspiRemote.Helpers
+{
+    public struct SystemInfoCommands
+    {
+        public const string Model = "cat /proc/cpuinfo | grep Model | awk -F ': ' '{print $2}'";
+        public const string Architecture = "uname -m";
+        public const string OSName = "cat /etc/os-release | grep -v PRETTY_NAME | grep NAME | awk -F '\"' '{print $2}'";
+        public const string OSVersion = "cat /etc/os-release | grep -v VERSION_ | grep VERSION | awk -F '\"' '{print $2}'";
+        public const string Kernel = "echo \"$(uname -rs)\" \"(\"\"$(getconf LONG_BIT)\" \"bit)\"";
+        public const string Hostname = "hostname";
+        public const string IPv4Addresses = "ip add show | grep -v \"inet 127.0.0.1\" | grep -v inet6 | grep inet | awk -F ' ' '{print $2}'";
+        public const string IPv6Addresses = "ip add show | grep -v \"inet6 ::1\" | grep inet6 | awk -F ' ' '{print $2}'";
+    }
+
+    public static class SystemInfoHelpers
+    {
+        public static string GetModel(SshClient sshClient) => ExecuteCommand(sshClient, SystemInfoCommands.Model);
+        public static string GetArchitecture(SshClient sshClient) => ExecuteCommand(sshClient, SystemInfoCommands.Architecture);
+        public static string GetOSName(SshClient sshClient) => ExecuteCommand(sshClient, SystemInfoCommands.OSName);
+        public static string GetOSVersion(SshClient sshClient) => ExecuteCommand(sshClient, SystemInfoCommands.OSVersion);
+        public static string GetKernel(SshClient sshClient) => ExecuteCommand(sshClient, SystemInfoCommands.Kernel);
+        public static string GetHostname(SshClient sshClient) => ExecuteCommand(sshClient, SystemInfoCommands.Hostname);
+        public static string GetIPv4Addresses(SshClient sshClient) => ExecuteCommand(sshClient, SystemInfoCommands.IPv4Addresses);
+        public static string GetIPv6Addresses(SshClient sshClient) => ExecuteCommand(sshClient, SystemInfoCommands.IPv6Addresses);
+
+        private static string ExecuteCommand(SshClient sshClient, string commandText)
+        {
+            var command = sshClient.RunCommand(commandText);
+
+            if (command.ExitStatus != 0 && command.Error.Length > 0)
+                throw new SshException(command.Error);
+            else if (command.ExitStatus != 0 && command.Result.Length > 0)
+                throw new SshException(command.Result);
+            else if (command.ExitStatus != 0)
+                throw new SshException("Error during loading data.");
+            else
+                return command.Result.Trim();
+        }
+    }
+}
--- RaspiRemote/ViewModels/SystemInfoPageViewModel.cs
+++ RaspiRemote/ViewModels/SystemInfoPageViewModel.cs
@@ -1,4 +1,7 @@
-﻿using Renci.SshNet;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using RaspiRemote.Helpers;
+using Renci.SshNet;
 
 namespace RaspiRemote.ViewModels
 {
@@ -6,9 +9,59 @@ namespace RaspiRemote.ViewModels
     {
         private readonly SshClient _sshClient;
 
+        [ObservableProperty]
+        private bool _isRefreshing;
+
+        [ObservableProperty]
+        private string _model;
+
+        [ObservableProperty]
+        private string _architecture;
+
+        [ObservableProperty]
+        private string _OSName;
+
+        [ObservableProperty]
+        private string _OSVersion;
+
+        [ObservableProperty]
+        private string _kernel;
+
+        [ObservableProperty]
+        private string _hostname;
+
+        [ObservableProperty]
+        private string _IPv4Addresses;
+
+        [ObservableProperty]
+        private string _IPv6Addresses;
+
+
         public SystemInfoPageViewModel(SshClientContainer sshClientContainer)
         {
             _sshClient = sshClientContainer.SshClient;
+
+            _ = InvokeAsyncWithLoader(LoadBasicInfo);
+        }
+
+        private async Task LoadBasicInfo() => await Task.Run(() =>
+        {
+            Model = SystemInfoHelpers.GetModel(_sshClient);
+            Architecture = SystemInfoHelpers.GetArchitecture(_sshClient);
+            OSName = SystemInfoHelpers.GetOSName(_sshClient);
+            OSVersion = SystemInfoHelpers.GetOSVersion(_sshClient);
+            Kernel = SystemInfoHelpers.GetKernel(_sshClient);
+            Hostname = SystemInfoHelpers.GetHostname(_sshClient);
+            IPv4Addresses = SystemInfoHelpers.GetIPv4Addresses(_sshClient);
+            IPv6Addresses = SystemInfoHelpers.GetIPv6Addresses(_sshClient);
+        });
+
+        [RelayCommand]
+        private async Task Refresh()
+        {
+            IsRefreshing = true;
+            await LoadBasicInfo();
+            IsRefreshing = false;
         }
     }
 }
