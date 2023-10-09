765af9a735963cc41e4dd49cb2147f2eb8f713b3
The-Mag-Pie <barteksor2000@onet.pl>
2023-10-10 01:24:19 +0200

    added icons in flyout for sensors, reconnect and disconnect + added shutdown and reboot options in flyout

--- RaspiRemote/AppShell.xaml.cs
+++ RaspiRemote/AppShell.xaml.cs
@@ -1,11 +1,59 @@
 ﻿using CommunityToolkit.Mvvm.Input;
 using RaspiRemote.Pages;
 using RaspiRemote.ViewModels;
+using System.Diagnostics;
 
 namespace RaspiRemote;
 
 public partial class ShellViewModel : BaseViewModel
 {
+    [RelayCommand]
+    private async Task Power(string option) => await InvokeAsyncWithLoader(async () =>
+    {
+        var sshContainer = ServiceHelper.GetService<SshClientContainer>();
+        var sshClient = sshContainer.SshClient;
+        var deviceInfo = sshContainer.DeviceInfo;
+
+        string command;
+
+        switch (option)
+        {
+            case "Shutdown":
+                command = "shutdown now";
+                break;
+
+            case "Reboot":
+                command = "shutdown -r now";
+                break;
+
+            default:
+                command = "echo \"Power option not specified\" && exit 1";
+                break;
+        }
+
+        try
+        {
+            //var resultCmd = sshClient.RunCommand($"echo \"{deviceInfo.Password}\" | sudo -S -k bash -c \"(sleep 3 && {command}) &\"");
+            var resultCmd = sshClient.RunCommand($"echo \"{deviceInfo.Password}\" | sudo -S -k {command}");
+
+            if (resultCmd.ExitStatus != 0 || resultCmd.Error.Length > 0)
+            {
+                throw new Renci.SshNet.Common.SshException($"{resultCmd.Error}{resultCmd.Result}");
+            }
+
+            await Disconnect();
+        }
+        catch (Renci.SshNet.Common.SshConnectionException ex)
+        {
+            Debug.WriteLine(ex.Message);
+            await Disconnect();
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", ex.Message, "OK");
+        }
+    });
+
 	[RelayCommand]
 	private async Task Reconnect() => await InvokeAsyncWithLoader(async () =>
 	{
