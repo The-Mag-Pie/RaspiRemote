7d95844122c57be0851b0428d42402e947d4ba11
The-Mag-Pie <barteksor2000@onet.pl>
2023-10-22 17:15:32 +0200

    sensors page: fixed error after deleting sensor on windows

--- RaspiRemote/ViewModels/Sensors/DHT11SensorViewModel.cs
+++ RaspiRemote/ViewModels/Sensors/DHT11SensorViewModel.cs
@@ -27,10 +27,8 @@ namespace RaspiRemote.ViewModels.Sensors
         {
             var cmd = _sshClient.CreateCommand($"~/raspiremote/ReadSensorData dht11 {(int)Pin}");
 
-            while (true)
+            while (_ct.IsCancellationRequested is false)
             {
-                _ct.ThrowIfCancellationRequested();
-
                 cmd.Execute();
                 var result = cmd.Result.Trim().Split("\n");
 
